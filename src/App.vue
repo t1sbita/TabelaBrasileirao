0d42b25b76c70fb2a3096b8c366ec2051594e075
Elias Garcia <elias.sgarcia@gmail.com>
2021-03-11 10:07:09 -0300

    Limpezae organização de código

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="green ligthen-3"
-      dense
-      dark
-    >
-
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-    </v-app-bar>  
+    <AppTopBar/>
     
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-format-list-bulleted</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-
-           <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-github</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>GitHub</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-main>
       <PaginaInicial/>
     </v-main>
@@ -64,17 +10,15 @@
 
 <script>
 import PaginaInicial from './components/PaginaInicial'
+import AppTopBar from './components/shared/AppTopBar'
 
 export default {
   name: 'App',
 
   components: {
     PaginaInicial,
+    AppTopBar
   },
 
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
 };
 </script>
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="app-top-bar">
+        <v-app-bar
+        app
+        color="green ligthen-3"
+        dense
+        dark
+        >
+
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        </v-app-bar>  
+        
+        <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+        >
+        <v-list
+            nav
+            dense
+        >
+            <v-list-item-group
+            v-model="group"
+            active-class="green--text text--accent-4"
+            >
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Início</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-format-list-bulleted</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>Classificação</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-icon>
+                <v-icon>mdi-github</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>GitHub</v-list-item-title>
+            </v-list-item>
+            </v-list-item-group>
+        </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTopBar',
+    
+    data: () => ({
+    drawer: false,
+    group: null,
+    }),
+
+}
+</script>
+
+<style>
+
+</style>
